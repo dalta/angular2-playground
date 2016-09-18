71c7cdf043870011a092521585b63782c11fe45f
dalta <vjtimca11@gmail.com>
2016-09-18 15:44:51 +0530

    Adds button click with event information. Also demos binging !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
     import { Component } from '@angular/core';
     @Component({
       selector: 'my-app',
-      template: '<h1>My First Angular 2 App</h1>'
+      template: '<h1>My {{count}} Angular 2 App</h1> <button (click)="this.increaseCount($event)">++Counter</button>'
     })
-    export class AppComponent { }
+    export class AppComponent { 
+      count : number = 1
+
+      increaseCount(event){
+        console.log("increaseCount..", event)
+        this.count+=1;
+      }
+    }
